bd20a5c9a8c2902403e91accd7a7cd562fe9e2bb
clintjeff2 <clintjeff2@gmail.com>
2025-05-27 12:19:48 +0100

    Adjusted component to be clickable and navigatable to new page

--- src/components/StoreCard.jsx
+++ src/components/StoreCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function StoreCard(props) {
+	console.log(props);
 	return (
-		<div className="store-card">
-			<div className="img-container">
-				<img src={props.logo} alt="Tasty Burger" />
-				<span>{props.name}</span>
-			</div>
-			<div className="store-details">
-				<div className="location-cat">
-					<span>{props.location}</span>
-					<span className="cat">{props.category[0].name}</span>
+		<Link className="link" to={`/store/${props.id}`}>
+			<div className="store-card">
+				<div className="img-container">
+					<img src={props.logo} alt="Tasty Burger" />
+					<div className="details-background"></div>
+					<span>{props.name}</span>
 				</div>
-				<p className="description">{props.description}</p>
-				<p>web.store.site</p>
+				<div className="store-details">
+					<div className="location-cat">
+						<span>{props.location}</span>
+						<span className="cat">{props.category[0].name}</span>
+					</div>
+					<p className="description">{props.description}</p>
+					<p>web.store.site</p>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
 

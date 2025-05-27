e70cba54454beaa2332af626f6c26b9fbd84d238
clintjeff2 <clintjeff2@gmail.com>
2025-05-27 12:22:05 +0100

    Added getStore function to get information for a particular store

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -4,6 +4,7 @@ export const AppContext = createContext();
 
 function AppProvider(props) {
 	const [allStores, setAllStores] = useState([]);
+	const [store, setStore] = useState(null);
 	const getAllStores = async () => {
 		try {
 			const response = await fetch('http://localhost:4000/store');
@@ -15,7 +16,16 @@ function AppProvider(props) {
 			console.log(err);
 		}
 	};
-	const data = { getAllStores, allStores };
+	const getStore = async (storeId) => {
+		try {
+			const res = await fetch(`http://localhost:4000/store/${storeId}`);
+			const data = await res.json();
+			setStore(data.store[0]);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	const data = { getAllStores, allStores, store, getStore };
 	return (
 		<AppContext.Provider value={data}>{props.children}</AppContext.Provider>
 	);
